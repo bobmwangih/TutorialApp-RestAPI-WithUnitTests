8bb7f800377c17d5f4e5dcf05d2b856be0e3ced1
bobmwangih <bobbymwangih@gmail.com>
2022-06-06 16:43:20 +0300

    Configuring Exception handling (RestControllerAdvice + Exception handler)

--- src/main/java/com/bob/exceptionHandling/ControllerExceptionHandler.java
+++ src/main/java/com/bob/exceptionHandling/ControllerExceptionHandler.java
@@ -3,25 +3,42 @@ package com.bob.exceptionHandling;
 import java.util.Date;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 
-@ControllerAdvice
+@RestControllerAdvice
 public class ControllerExceptionHandler {
+//Using @controllerAdvice
+	/*
+	 * @ExceptionHandler(ResourceNotFoundException.class) public
+	 * ResponseEntity<ErrorMessage>
+	 * resourceNotFoundException(ResourceNotFoundException ex, WebRequest request) {
+	 * ErrorMessage message = new ErrorMessage(HttpStatus.NOT_FOUND.value(), new
+	 * Date(), ex.getMessage(), request.getDescription(false)); return new
+	 * ResponseEntity<ErrorMessage>(message, HttpStatus.NOT_FOUND); }
+	 * 
+	 * @ExceptionHandler(Exception.class) public ResponseEntity<ErrorMessage>
+	 * globalExceptionHandler(Exception ex, WebRequest request) { ErrorMessage
+	 * message = new ErrorMessage(HttpStatus.INTERNAL_SERVER_ERROR.value(), new
+	 * Date(), ex.getMessage(), request.getDescription(false)); return new
+	 * ResponseEntity<ErrorMessage>(message, HttpStatus.INTERNAL_SERVER_ERROR); }
+	 */
 
+//Using @RestControllerAdvice
 	@ExceptionHandler(ResourceNotFoundException.class)
-	public ResponseEntity<ErrorMessage> resourceNotFoundException(ResourceNotFoundException ex, WebRequest request) {
+	@ResponseStatus(value = HttpStatus.NOT_FOUND)
+	public ErrorMessage resourceNotFoundException(ResourceNotFoundException ex, WebRequest request) {
 		ErrorMessage message = new ErrorMessage(HttpStatus.NOT_FOUND.value(), new Date(), ex.getMessage(),
 				request.getDescription(false));
-		return new ResponseEntity<ErrorMessage>(message, HttpStatus.NOT_FOUND);
+		return message;
 	}
-
+	
 	@ExceptionHandler(Exception.class)
-	public ResponseEntity<ErrorMessage> globalExceptionHandler(Exception ex, WebRequest request) {
-		ErrorMessage message = new ErrorMessage(HttpStatus.INTERNAL_SERVER_ERROR.value(), new Date(), ex.getMessage(),
-				request.getDescription(false));
-		return new ResponseEntity<ErrorMessage>(message, HttpStatus.INTERNAL_SERVER_ERROR);
+	@ResponseStatus(value =HttpStatus.INTERNAL_SERVER_ERROR)
+	public ErrorMessage globalExceptionHandler(Exception ex,WebRequest request) {
+		ErrorMessage message = new ErrorMessage(HttpStatus.INTERNAL_SERVER_ERROR.value(), new Date(), ex.getMessage(), request.getDescription(false));
+		return message;
 	}
 }
